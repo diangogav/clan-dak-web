d22c2d6afb3b779157e49d03da638d0b7980f042
DiangoGav <diangogavidia@gmail.com>
2023-04-07 22:40:35 -0400

    text decoration none for navbar links

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -1,24 +1,26 @@
-import { AppBar, Box, Button, Link, Toolbar, Typography } from "@mui/material"
-import NextLink from 'next/link'
+import { AppBar, Box, Button, Link, Toolbar, Typography } from "@mui/material";
+import NextLink from "next/link";
 export const NavBar = () => {
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <NextLink href='/' passHref legacyBehavior>
-          <Link> Clan DAK </Link>
+        <NextLink href="/" passHref legacyBehavior>
+          <Link sx={{ textDecoration: "none" }}>
+            {" "}
+            <Typography>Clan DAK </Typography>
+          </Link>
         </NextLink>
 
         <Box flex={1} />
 
         <Box>
           <NextLink href="/events" passHref legacyBehavior>
-            <Link>
+            <Link sx={{ textDecoration: "none" }}>
               <Button>Eventos</Button>
             </Link>
           </NextLink>
         </Box>
-
       </Toolbar>
     </AppBar>
   );
-}
+};
